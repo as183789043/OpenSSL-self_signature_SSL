7aae88de300b3ad258aeaee6f9cf95d0e55627f4
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-05-13 17:58:27 +0800

    Create certificate.sh

---
+++ certificate.sh
@@ -0,0 +1,17 @@
+if  [ $(find . -name "*.cnf" | wc -l) != 2 ]; then
+        echo "Check you must have 2 cnf file in this directory"
+        exit 0
+fi
+
+openssl genrsa -des3 -out rootCA.key 4096
+
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 36500 -out rootCA.crt -config ca.cnf
+
+openssl genrsa -out example.key 4096
+
+openssl req -new -sha256 -nodes -key example.key -out example.csr -config  example.cnf
+
+openssl x509 -req -in example.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out example.crt -days 3650 -sha512
+
+
+echo "Generated Certificate Done!"
