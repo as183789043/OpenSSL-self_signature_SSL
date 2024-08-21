84bfcf7994ce29bc8302abaa9c9d64dcb12bd1bc
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-08-21 17:37:25 +0800

    Modify Subject Alternative Name (SAN) Script

--- certificate.sh
+++ certificate.sh
@@ -11,7 +11,7 @@ openssl genrsa -out example.key 4096
 
 openssl req -new -sha256 -nodes -key example.key -out example.csr -config  example.cnf
 
-openssl x509 -req -in example.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out example.crt -days 3650 -sha512
-
+#openssl x509 -req -in example.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out example.crt -days 3650 -sha512
+openssl x509 -req -in example.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out example.crt -days 3650 -sha512 -extfile example.cnf -extensions v3_req
 
 echo "Generated Certificate Done!"
